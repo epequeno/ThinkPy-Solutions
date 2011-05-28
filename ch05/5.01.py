4e6d1a43aeb6e98a6cf65770a170aaaca2fce4e4
Estevan Pequeno <estevan.pequeno@gmail.com>
2011-05-27 22:18:25 -0500

    non-printing chars

--- ch05/5.01.py
+++ ch05/5.01.py
@@ -1,13 +1,14 @@
-# Fermat’s Last Theorem says that there are no integers a, b, and c such that
+# Fermat's Last Theorem says that there are no integers a, b, and c such that
 # a^n + b^n = c^n
 # for any values of n greater than 2.
-# 1. Write a function named check_fermat that takes four parameters—a, b, c and n—and that
-# checks to see if Fermat’s theorem holds. If n is greater than 2 and it turns out to be true that
-# a^n + b^n = c^n
-# the program should print, “Holy smokes, Fermat was wrong!” Otherwise the program should
-# print, “No, that doesn’t work.”
-# 2. Write a function that prompts the user to input values for a, b, c and n, converts them to
-# integers, and uses check_fermat to check whether they violate Fermat’s theorem.
+# 1. Write a function named check_fermat that takes four parameters-a, b, c 
+# and n-and that checks to see if Fermat's theorem holds. If n is greater than 
+# 2 and it turns out to be true that a^n + b^n = c^n
+# the program should print, "Holy smokes, Fermat was wrong!" Otherwise the
+# program should  print, "No, that doesn't work."
+# 2. Write a function that prompts the user to input values for a, b, c and n,
+# converts them to integers, and uses check_fermat to check whether they 
+# violate Fermat's theorem.
 
 a = int(raw_input('What value to use for a?\n'))
 b = int(raw_input('What value to use for b?\n'))
@@ -15,6 +16,9 @@ c = int(raw_input('What value to use for c?\n'))
 n = int(raw_input('What value to use for n?\n'))
 
 def check_fermat(a, b, c, n):
+    if n == 2:
+       print 'Pythagoras got that one already, thanks.\n'
+       return
     if a**n + b**n == c**n:
         print('Holy Smokes, Fermat was Wrong!')
     else:
