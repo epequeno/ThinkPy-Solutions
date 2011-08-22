9ec993a8c8e685aaa8ea1da3cbe867dd585bd1be
Estevan Pequeno <estevan.pequeno@gmail.com>
2011-08-21 19:32:41 -0500

    Edited ch09/9.07.py via GitHub

--- ch09/9.07.py
+++ ch09/9.07.py
@@ -15,8 +15,8 @@ def is_trip_pair(wordList):
    for word in wordList:
        i = 0
        pairCount = 0
-       while i < len(word)-1:
-           if word[i] == word[i+1]:
+       while i < len(word) - 1:
+           if word[i] == word[i + 1]:
                pairCount += 1
                if pairCount == 3:
                    print word
