76eeea07776e7fffaf12ebbe8d9bdda490632d1c
Estevan Pequeno <estevan.pequeno@gmail.com>
2011-11-22 17:07:44 -0600

    Update ch10/10.09.py

--- ch10/10.09.py
+++ ch10/10.09.py
@@ -1,8 +1,23 @@
 # Two words are a "reverse pair" if each is the reverse of the other. Write
 # a program that finds all the reverse pairs in the word list.
 #
-def is_reverse():
-   if word forward == word backward:
-       print True
-   else: 
-       print False
+word_file = open('words.txt')
+
+def make_list():
+    word_list = []
+    for word in word_file:
+        word = word[:-2]
+        word_list.append(word)
+    return word_list
+    
+word_list = make_list()
+
+def find_rev_pairs(word_list):
+    results = []
+    for word in word_list:
+        if word[::-1] in word_list:
+            results.append(word)
+            word_list.remove(word[::-1])
+    return results
+            
+print find_rev_pairs(word_list)
