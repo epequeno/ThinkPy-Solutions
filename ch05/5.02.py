3965cfcd0cf2c86e8b1ae329f290987426455d42
Estevan Pequeno <estevan.pequeno@gmail.com>
2011-11-30 19:21:46 -0600

    Update ch05/5.02.py

--- ch05/5.02.py
+++ ch05/5.02.py
@@ -1,22 +1,17 @@
-#a = raw_input('Side a?\n')
-#b = raw_input('Side b?\n')
-#c = raw_input('Side c?\n')
-#
-#def is_triangle(a, b, c):
-#   for i in [a, b, c]:
-#       i = float(i)
-#       print i
-#       print a, b, c
-#   if (a > (b + c) or
-#       b > (a + c) or
-#       c > (a + b)):
-#       print 'Not gonna happen buddy'
-#   else:
-#       print 'You got yourself a triangle there.'
+# If you are given three sticks, you may or may not be able to arrange 
+# them in a triangle. For example, if one of the sticks is 12 inches long and 
+# the other two are one inch long, it is clear that you will not be able to get the 
+# short sticks to meet in the middle. For any three lengths, there is a simple
+# test to see if it is possible to form a triangle:
+# “If any of the three lengths is greater than the sum of the other two, then 
+# you cannot form a triangle. Otherwise, you can.”
+# 1. Write a function named is_triangle that takes three integers as arguments, and
+# that prints either “Yes” or “No,” depending on whether you can or cannot form a 
+# triangle from sticks with the given lengths.
+# 2. Write a function that prompts the user to input three stick lengths, converts 
+# them to integers, and uses is_triangle to check whether sticks with the given 
+# lengths can form a triangle.
 
-#is_triangle(a, b ,c)
-
-# version 2:
 a = int(raw_input("A = "))
 b = int(raw_input("B = "))
 c = int(raw_input("C = "))
