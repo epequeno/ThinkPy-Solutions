bbe474391560a3f6136b3f6d49ba209dfd771b9b
Estevan Pequeno <estevan.pequeno@gmail.com>
2011-12-27 14:12:34 -0600

    12.04 part3 completed

--- ch12/12.04.py
+++ ch12/12.04.py
@@ -13,7 +13,7 @@
 # first, followed by the second largest set, and so on.
 # 3. In Scrabble a "bingo" is when you play all seven tiles in your rack, along
 # with a letter on the board, to form an eight-letter word. What set of 8 
-# letters forms the most possible bingos?
+# letters forms the most possible bingos? Answer: 'aeginrst'
 # Hint: there are seven.
 # 4. Two words form a "metathesis pair" if you can transform one into the other
 # by swapping two letters; for example, "converse" and "conserve." Write a 
@@ -27,6 +27,7 @@ file_of_words = open('words.txt')
 file_of_sorted_words = open('list_of_sorted_words.txt')
 
 def make_word_list():
+    '''Create a list from the txt file of dictionary words '''
     word_list= []
     for word in file_of_words:
         word_list.append(word.strip('\n'))
@@ -35,6 +36,9 @@ def make_word_list():
 word_list = make_word_list()
 
 def make_sorted_word_list():
+    '''Create list from the txt file of dictionary words that have had
+    thier individual characters sorted alphabetically (to check anagrams
+    against)'''
     sorted_word_list = []
     for sorted_word in file_of_sorted_words:
         sorted_word_list.append(sorted_word.strip('\n'))
@@ -42,10 +46,8 @@ def make_sorted_word_list():
 
 sorted_word_list = make_sorted_word_list()
 
-def is_anagram(sorted_word, test_word):
-    return sorted(sorted_word) == sorted(test_word)
-   
 def make_dict():
+    '''Create dictionary that masp from sorted words to empty lists '''
     sorted_to_anagrams = {}
     for sorted_word in sorted_word_list:
         sorted_to_anagrams[sorted_word] = []
@@ -54,6 +56,10 @@ def make_dict():
 sorted_to_anagrams = make_dict()
 
 def match_sorted_word_to_anagram():
+    '''Checks if a sorted word from the word_list is already in the dictionary
+    if so, append the unsorted word to the list (value) for the key (sorted
+    word). Returns only words which have at least 1 anagram (>= 2 words in 
+    a list)'''
     for word in word_list:
         temp_word = sorted(word)
         temp_word = ''.join(temp_word)
@@ -68,10 +74,21 @@ def match_sorted_word_to_anagram():
 final_dict = match_sorted_word_to_anagram()
 
 def make_sorted_list_of_anagrams():
+    '''Make a list of lists of anagrams sorted by length in reverse order'''
     sorted_by_length_list = []
     for key in final_dict:
         sorted_by_length_list.append(final_dict[key])
     sorted_by_length_list.sort(key=len, reverse=True)
     return sorted_by_length_list
     
-print make_sorted_list_of_anagrams()
+def find_bingos():
+    ''' Find and return the longest list of anagrams whose word length=8'''
+    bingos = []
+    for key in final_dict:
+        if len(key) == 8:
+            bingos.append(final_dict[key])
+    bingos.sort(key=len, reverse=True)
+    return bingos[0]
+    
+# print find_bingos()
+
