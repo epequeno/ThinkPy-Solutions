d8c70cee57471732c6beacc0c82de4321b51e729
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-01-01 17:25:49 -0600

    ch15 done

--- ch15/15.02.py
+++ ch15/15.02.py
@@ -2,3 +2,43 @@
 # named dx and dy. It should change the location of the rectangle by adding 
 # dx to the x coordinate of corner and adding dy to the y coordinate of corner.
 
+class Point(object):
+    """Represents a point in 2d space"""
+    
+class Rectangle(object):
+    """Represents a rectangle in 2d space"""
+    
+rectangle = Rectangle()
+
+bottom_left = Point()
+bottom_left.x = 3.0
+bottom_left.y = 5.0
+
+top_right = Point()
+top_right.x = 5.0
+top_right.y = 10.0
+
+rectangle.corner1 = bottom_left
+rectangle.corner2 = top_right
+
+dx = 5.0
+dy = 12.0
+    
+def move_rectangle(rectangle, dx, dy):
+    """Takes a rectangle and moves it to the values of dx and dy."""
+    print ("The rectangle started with bottom left corner at (%g,%g)" 
+           % (rectangle.corner1.x, rectangle.corner1.y)),
+    print ("and top right corner at (%g,%g)." 
+           % (rectangle.corner2.x, rectangle.corner2.y)),
+    print "dx is %g and dy is %g" % (dx, dy)
+    rectangle.corner1.x = rectangle.corner1.x + dx
+    rectangle.corner2.x = rectangle.corner2.x + dx
+    rectangle.corner1.y = rectangle.corner1.y + dy
+    rectangle.corner2.y = rectangle.corner2.y + dy
+    print ("It ended with a bottom left corner at (%g,%g)" 
+           % (rectangle.corner1.x, rectangle.corner1.y)),
+    print ("and a top right corner at (%g,%g)" 
+           % (rectangle.corner2.x, rectangle.corner2.y))
+    
+    
+move_rectangle(rectangle, dx, dy)
--- ch15/15.03.py
+++ ch15/15.03.py
@@ -1,3 +1,41 @@
 # Write a version of move_rectangle that creates and returns a new Rectangle
 # instead of modifying the old one.
 
+import copy
+
+class Point(object):
+    """Represents a point in 2d space"""
+    
+class Rectangle(object):
+    """Represents a rectangle in 2d space"""
+    
+rectangle = Rectangle()
+
+bottom_left = Point()
+bottom_left.x = 3.0
+bottom_left.y = 5.0
+
+top_right = Point()
+top_right.x = 5.0
+top_right.y = 10.0
+
+rectangle.corner1 = bottom_left
+rectangle.corner2 = top_right
+
+dx = 5.0
+dy = 12.0
+
+def move_rectangle(rectangle, dx, dy):
+    """Moves a trangle to the values of dx and dy using deepcopy to create
+    a new rectangle object and not modify the original rectangle."""
+    new_rectangle = copy.deepcopy(rectangle)
+    print ("Original: (%g,%g)" % (rectangle.corner1.x, rectangle.corner1.y)),
+    print ("(%g,%g)" % (rectangle.corner2.x, rectangle.corner2.y))
+    new_rectangle.corner1.x = new_rectangle.corner1.x + dx
+    new_rectangle.corner2.x = new_rectangle.corner2.x + dx
+    new_rectangle.corner1.y = new_rectangle.corner1.y + dy
+    new_rectangle.corner2.y = new_rectangle.corner2.y + dy
+    print ("New: (%g,%g)" % (new_rectangle.corner1.x, new_rectangle.corner1.y)),
+    print ("(%g,%g)" % (new_rectangle.corner2.x, new_rectangle.corner2.y))
+    
+move_rectangle(rectangle, dx, dy)
