233e56a1fe1c58f73a1d9678b652a863d083a760
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-01-01 17:42:45 -0600

    starting on ch16

--- ch16/16.01.py
+++ ch16/16.01.py
@@ -2,3 +2,16 @@
 # in the form hour:minute:second. Hint: the format sequence '%.2d' prints
 # an integer using at least two digits, including a leading zero if necessary.
 
+class Time(object):
+    """ represents the time of day.
+    attributes: hour, minute, second"""
+    
+time = Time()
+time.hour = 11
+time.minute = 59
+time.second = 30
+
+def print_time(time):
+    print "%.2d:%.2d:%.2d" % (time.hour, time.minute, time.second)
+
+print_time(time)
