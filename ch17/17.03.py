c25f8e215d10d719c50902d9a3f236c2842b2801
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-01-02 18:31:50 -0600

    progress on ch17

--- ch17/17.03.py
+++ ch17/17.03.py
@@ -1,2 +1,18 @@
 # Write a str method for the Point class. Create a Point object and print it.
 
+class Point(object):
+    def __init__ (self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        return '(%d, %d)' % (self.x, self.y)
+        
+point = Point()
+print point
+
+point = Point(10)
+print point
+
+point = Point(10, 15)
+print point
--- ch17/17.04.py
+++ ch17/17.04.py
@@ -1,2 +1,20 @@
 # Write an add method for the Point class.
 
+class Point(object):
+    def __init__ (self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        return '(%d, %d)' % (self.x, self.y)
+        
+    def __add__(self, other):
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        new_point = Point(new_x, new_y)
+        return new_point
+        
+point1 = Point(1, 3)
+point2 = Point(4, 5)
+
+print point1 + point2
