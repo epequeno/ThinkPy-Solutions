d3913b8e1acbad98f8ec9468280853de39f4f074
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-01-02 18:06:01 -0600

    starting on ch17

--- ch17/17.01.py
+++ ch17/17.01.py
@@ -2,3 +2,15 @@
 # appropriate to rewrite int_to_time as a method; it's not clear what object
 # you would invoke it on!
 
+class Time(object):
+    def time_to_int(self):
+        minutes = time.hour * 60 + time.minute
+        seconds = minutes * 60 + time.second
+        return seconds
+        
+time = Time()
+time.hour = 11
+time.minute = 59
+time.second = 30
+
+print time.time_to_int()
--- ch17/17.02.py
+++ ch17/17.02.py
@@ -1,3 +1,20 @@
 # Write an init method for the Point class that takes x and y as optional
 # parameters and assigns them to the corresponding attributes.
 
+class Point(object):
+    def __init__ (self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+        
+    def print_point(self):
+        print "x =", self.x, ",",
+        print "y =", self.y
+        
+point = Point()
+point.print_point()
+
+point = Point(10)
+point.print_point()
+
+point = Point (20, 30)
+point.print_point()
