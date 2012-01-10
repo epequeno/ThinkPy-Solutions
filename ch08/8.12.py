000a554a5f65ef42977045c0c9edffd38988b996
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-01-09 22:01:25 -0600

    finally worked the bugs out of 8.12

--- ch08/8.12.py
+++ ch08/8.12.py
@@ -20,13 +20,25 @@ Created on Sun Aug  7 19:24:25 2011
 
 # Current Status: Complete
 
-def rot13(word, amount):
-   rotated = ''
-   for letter in word:
-       rotated += chr(ord(letter) + amount)
-   return rotated
-
-print rot13("melon", -10)
-
-print ord('a')
-print ord('z')
+def normalize(x):
+    if x > 122:
+        while x > 122:
+            x -= 26
+    elif x < 97:
+        while x < 97:
+            x += 26
+    return x
+    
+def rotate_word(word, amount):
+    new_word = ''
+    for letter in word:
+        letter = letter.lower()
+        if ord(letter) + amount > 122:
+            new_word += chr(normalize(ord(letter) + amount))
+        elif ord(letter) + amount < 97:
+            new_word += chr(normalize(ord(letter) + amount))
+        else:
+            new_word += chr(ord(letter) + amount)
+    return new_word
+    
+print rotate_word("Melon", -10)
