3a7c3e3e4025c5791e5b3c47254868d7b9540aa3
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-02-24 21:47:55 -0600

    fixing ch13

--- ch13/13.03.py
+++ ch13/13.03.py
@@ -1,55 +1,60 @@
-#13.3  Word histogram
-#
-#Here is a program that reads a file and builds a histogram of the words in 
-#the file:
-#
-#import string
-#
-#def process_file(filename):
-#    h = dict()
-#    fp = open(filename)
-#    for line in fp:
-#        process_line(line, h)
-#    return h
-#
-#def process_line(line, h):
-#    line = line.replace('-', ' ')
-#    
-#    for word in line.split():
-#        word = word.strip(string.punctuation + string.whitespace)
-#        word = word.lower()
-#
-#        h[word] = h.get(word, 0) + 1
-#
-#hist = process_file('emma.txt')
-#This program reads emma.txt, which contains the text of Emma by Jane Austen.
-#
-#process_file loops through the lines of the file, passing them one at a time
-# to process_line. The histogram h is being used as an accumulator.
-#
-#
-#process_line uses the string method replace to replace hyphens with spaces
-# before using split to break the line into a list of strings. It traverses
-# the list of words and uses strip and lower to remove punctuation and convert
-# to lower case. (It is a shorthand to say that strings are "converted;" 
-# remember that string are immutable, so methods like strip and lower return
-# new strings.)
-#
-#Finally, process_line updates the histogram by creating a new item or 
-#incrementing an existing one.
-#
-#
-#To count the total number of words in the file, we can add up the
-# frequencies in the histogram:
-#
-#def total_words(h):
-#    return sum(h.values())
-#The number of different words is just the number of items in the dictionary:
-#def different_words(h):
-#    return len(h)
-#Here is some code to print the results:
-#print 'Total number of words:', total_words(hist)
-#print 'Number of different words:', different_words(hist)
-#And the results:
-#Total number of words: 161073
-#Number of different words: 7212
+#Exercise 3   Modify the program from the previous exercise to print the 20
+# most frequently-used words in the book.
+
+import string
+
+punctuations = [mark for mark in string.punctuation]
+whitespaces = [space for space in string.whitespace]
+
+origin = open('origin.txt', 'r')
+huck = open('huck.txt', 'r')
+frank = open('frank.txt', 'r')
+great = open('great.txt', 'r')
+meta = open('meta.txt', 'r')
+sherlock = open('sherlock.txt', 'r')
+tale = open('tale.txt', 'r')
+
+books = [origin, huck, frank, great, meta, sherlock, tale]
+
+def words(book):
+    list_ = []
+    flag = False
+    signal = "*** START OF"
+    for line in book:
+        if flag == True:
+            for word in line.split():
+                list_.append(word)
+        elif (signal in line) and (flag == False):
+            flag = True
+        else:
+            pass
+    return list_
+    
+def clean(word):
+    result = ''
+    for char in word:
+        if (char in whitespaces) or (char in punctuations):
+            pass
+        else:
+            result += char.lower()
+    return result
+
+def histogram(data):
+    hist = {}
+    for word in data:
+        hist[word] = hist.get(word, 0) + 1
+    return hist
+
+def stats():
+    for book in books:
+        print "Stats for %s:" % book.name
+        data = [clean(word) for word in words(book)]
+        print " Total: %s" % len(data)
+        hist = histogram(data)
+        print " Unique: %s" % len(hist)
+        list_ = [(hist[k], k) for k in hist]
+        list_.sort(reverse=True)
+        print " The top20 were:"
+        for i in range(0, 20):
+            print " %s)" % (i + 1), list_[i][1]
+stats()
--- ch13/13.04.py
+++ ch13/13.04.py
@@ -1,30 +1,43 @@
-#13.4  Most common words
-#
-#To find the most common words, we can apply the DSU pattern; most_common
-# takes a histogram and returns a list of word-frequency tuples, sorted in
-# reverse order by frequency:
-#
-#def most_common(h):
-#    t = []
-#    for key, value in h.items():
-#        t.append((value, key))
-#
-#    t.sort(reverse=True)
-#    return t
-#Here is a loop that prints the ten most common words:
-#t = most_common(hist)
-#print 'The most common words are:'
-#for freq, word in t[0:10]:
-#    print word, '\t', freq
-#And here are the results from Emma:
-#The most common words are:
-#to      5242
-#the     5204
-#and     4897
-#of      4293
-#i       3191
-#a       3130
-#it      2529
-#her     2483
-#was     2400
-#she     2364
+#Modify the previous program to read a word list (see Section 9.1) and then
+# print all the words in the book that are not in the word list. How many of
+# them are typos? How many of them are common words that should be in the word
+# list, and how many of them are really obscure?
+
+import string
+
+punctuations = [mark for mark in string.punctuation]
+whitespaces = [space for space in string.whitespace]
+
+origin = open('origin.txt', 'r')
+huck = open('huck.txt', 'r')
+frank = open('frank.txt', 'r')
+great = open('great.txt', 'r')
+meta = open('meta.txt', 'r')
+sherlock = open('sherlock.txt', 'r')
+tale = open('tale.txt', 'r')
+
+books = [origin, huck, frank, great, meta, sherlock, tale]
+
+def words(book):
+    list_ = []
+    flag = False
+    signal = "*** START OF"
+    for line in book:
+        if flag == True:
+            for word in line.split():
+                list_.append(word)
+        elif (signal in line) and (flag == False):
+            flag = True
+        else:
+            pass
+    return list_
+    
+def clean(word):
+    result = ''
+    for char in word:
+        if (char in whitespaces) or (char in punctuations):
+            pass
+        else:
+            result += char.lower()
+    return result
+
