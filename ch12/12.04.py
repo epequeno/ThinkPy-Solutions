face7f2a278b442cd3273b36633491d021714892
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-05-03 21:57:10 -0500

    progress on 12.4

--- ch12/12.04.py
+++ ch12/12.04.py
@@ -23,17 +23,11 @@
 
 # Status: Complete
 
-words_file = open('words.txt', 'r')
-
-def clean(myfile):
-    '''Removes newlines from file read.'''
-    return [line.strip('\r\n') for line in myfile]
-
-words_list = clean(words_file)
+words_list = [line.strip('\r\n') for line in open('words.txt', 'r')]
 
 def make_anagram_dict(mylist):
     '''Main grunt work. Returns dict which maps fingerprints to anagrams based 
-    on that fingerprint.Iterates through the list of words once to create a 
+    on that fingerprint. Iterates through the list of words once to create a 
     dict with the fingerprints. Iterates again to append matches to the 
     fingerprints dict. Finally, iterating through the dict to filter out 
     fingerprints that produced a list of length 1'''
@@ -42,25 +36,23 @@ def make_anagram_dict(mylist):
         fp = ''.join(sorted(word))
         if fp not in fingerprints:
             fingerprints[fp] = []
-    
-    for word in mylist:
-        fp = ''.join(sorted(word))
+        
         if fp in fingerprints:
             fingerprints[fp].append(word)
     
-    return_dict = dict()
+    filtered_dict = dict()
     for fp in fingerprints:
         if len(fingerprints[fp]) <= 1:
             pass
         else:
-            return_dict[fp] = fingerprints[fp]
+            filtered_dict[fp] = fingerprints[fp]
 
-    return return_dict
+    return filtered_dict
 
 words_dict = make_anagram_dict(words_list)
 
 def print_anagrams(mydict):
-    '''Uses a genrator to cal and print 5 items from mydict'''
+    '''Uses a genrator to call and print 5 items from mydict'''
     fp = (fp for fp in mydict)
 
     print "Sample from anagram dict:"
@@ -86,7 +78,7 @@ def sort_anagrams(mydict):
 
     print "Most anagrams:"
     for i in range(0, 5):
-        print "%s) " % (i + 1), anagrams_lists[i]
+        print "%s)" % (i + 1), anagrams_lists[i]
     print "..."
     print "\n"
 
@@ -101,7 +93,8 @@ def find_bingos(mydict):
 
     print "Top Bingos:"
     for i in range(0, 5):
-        print "%s) " % (i + 1), candidates[i]
+        fp = ''.join(sorted(candidates[i][0]))
+        print "%s) %s:" % ((i + 1), fp), candidates[i]
 
     print "..."
     print "\n"
@@ -113,7 +106,7 @@ def is_metathesis(reference, test):
      Caution: This function assumes strings of equal length'''
     i = 0
     count = 0
-    while i < (len(reference) - 1):
+    while i <= (len(reference) - 1):
         if reference[i] != test[i]:
             count += 1
         i += 1
@@ -135,7 +128,7 @@ def find_metathesis(mydict):
     
     print "Sample of metathesis pairs:"
     for i in range(0, 5):
-        print "%s) " % (i + 1), answer[i]
+        print "%s)" % (i + 1), answer[i]
     print "..."
 
 find_metathesis(words_dict)
