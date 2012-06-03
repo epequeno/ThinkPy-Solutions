b9e1b09898459ac743aa94b6fb62f2443c58bf48
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-06-03 00:31:54 -0500

    cleaned up 13.3

--- ch13/13.03.py
+++ ch13/13.03.py
@@ -1,8 +1,6 @@
 #Exercise 3   Modify the program from the previous exercise to print the 20
 # most frequently-used words in the book.
 
-#TODO: fix rest of file to work with new origin aliases
-
 import string
 
 punctuations = [mark for mark in string.punctuation]
@@ -33,34 +31,38 @@ def words(book):
             pass
     op.close()
     return list_
+    
   
-myList = words(origin)
+def clean(word):
+     result = ''
+     for char in word:
+         if (char in whitespaces) or (char in punctuations):
+             pass
+         else:
+             result += char.lower()
+     return result
 
-# def clean(word):
-#     result = ''
-#     for char in word:
-#         if (char in whitespaces) or (char in punctuations):
-#             pass
-#         else:
-#             result += char.lower()
-#     return result
+def histogram(data):
+     hist = {}
+     for word in data:
+         if word == '':
+             pass
+         else:
+             hist[word] = hist.get(word, 0) + 1
+     return hist
 
-# def histogram(data):
-#     hist = {}
-#     for word in data:
-#         hist[word] = hist.get(word, 0) + 1
-#     return hist
+def main():
+    for book in books:
+        data = [clean(word) for word in words(book)]
+        print "Stats for %s:" % book
+        hist = histogram(data)
+        top20 = []
+        for key in hist:
+            top20.append([hist[key], key])
+        top20.sort(reverse=True)
+        for i in range(0, 20):
+            print "  %s) %s %s" % (i + 1, top20[i][1], top20[i][0])
+        
+        print "\n"        
 
-# def stats():
-#     for book in books:
-#         print "Stats for %s:" % book.name
-#         data = [clean(word) for word in words(book)]
-#         print " Total: %s" % len(data)
-#         hist = histogram(data)
-#         print " Unique: %s" % len(hist)
-#         list_ = [(hist[k], k) for k in hist]
-#         list_.sort(reverse=True)
-#         print " The top20 were:"
-#         for i in range(0, 20):
-#             print " %s)" % (i + 1), list_[i][1]
-# stats()
+main()
