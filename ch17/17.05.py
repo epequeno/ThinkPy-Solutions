1f1e8f7ed833c07d37da6861d2862c7cd19d8aaf
Estevan Pequeno <estevan.pequeno@gmail.com>
2012-06-26 14:38:09 -0500

    sneaky little bug

--- ch17/17.05.py
+++ ch17/17.05.py
@@ -18,11 +18,11 @@ class Point(object):
         point_ = Point()
         if isinstance(other, Point):
             point_.x += self.x + other.x
-            point_.y += self.x + other.y
+            point_.y += self.y + other.y
             return point_
         elif type(other) == tuple:
             point_.x += self.x + other[0] 
-            point_.y += self.x + other[1]
+            point_.y += self.y + other[1]
         return point_
     
     def __radd__(self, other):
@@ -31,11 +31,8 @@ class Point(object):
     def __str__(self):
         return "(%s, %s)" % (self.x, self.y)
         
-point1 = Point(3, 4)
-point2 = (4, 3)
+point1 = Point(1, 6)
+point2 = (5, 2)
 point3 = point1 + point2
 point4 = point2 + point1
 print point3, point4
-
-#output
-#(7, 6) (7, 6)
