29c762e2213ad28d5b09d3bc217ac33804f7d59d
Estevan Pequeno <estevan.pequeno@gmail.com>
2013-01-16 01:16:22 -0600

    9.1 was still wrong =/

--- ch09/9.01.py
+++ ch09/9.01.py
@@ -10,11 +10,12 @@ def words(word):
     wordCount = 0
     lineCount = 0
     for word in wordList:
+        word = word.rstrip('\r\n')
         if len(word) > 20:
-            print word.rstrip('\r\n')
+            print word
             wordCount += 1
         lineCount += 1
     percent = (float(wordCount) / float(lineCount)) * 100.0
-    print "%0.3f%%" % percent
+    print "%0.4f%%" % percent
 
 words(wordList)
