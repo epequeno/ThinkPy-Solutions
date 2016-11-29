e4e4734ed02784da79d87d1c9c8e6c92ce9bd84f
Brent <bknox95@yahoo.com>
2016-11-29 18:31:28 -0500

    Update 19.02
    Solves the problem by creating a "Click Me for Graph" button that is linked to a canvas that has a red oval drawn on it.

--- ch19/19.02.py
+++ ch19/19.02.py
@@ -1,4 +1,16 @@
 # Write a program that creates a Canvas and a Button. When the user presses 
 # the Button, it should draw a circle on the canvas.
 
-# Current Status: Incomplete
+# Current Status: Complete
+from tkinter import *
+from tkinter import Canvas
+
+top = Tk()
+top.geometry("300x250")
+def circleWindow():
+    C = Canvas(top,bg='blue', height=250, width=300)
+    oval = C.create_oval(70, 30, 200, 200, fill="Red")
+    C.pack()
+b = Button(top, text= "Click me for Graph", command = circleWindow)
+b.place(x=90,y=100)
+top.mainloop()
